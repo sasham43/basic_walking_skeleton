079c1446130b29fe1987ddf60d05e238ef2d8540
sasham43 <sashasemail@gmail.com>
2016-04-09 09:58:31 -0500

    Skeleton is walking

---
+++ server/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var router = express.Router();
+
+mongoose.connect('mongodb://localhost/basic_walking_skeleton');
+
+var Cat = mongoose.model('Cat', {name: String});
+
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../public/views/index.html'));
+  console.log('GET request received');
+});
+
+router.post('/add', function(req, res, next){
+  var kitty = new Cat({name: req.body.name});
+  kitty.save(function(err){
+    if (err) console.log('meow %s', err);
+    res.send(kitty.toJSON());
+    next();
+  })
+});
+
+router.get('/cats', function(req, res, next){
+  return Cat.find({}).exec(function(err, cats){
+    if (err) throw new Error(err);
+    res.send(JSON.stringify(cats));
+    next();
+  })
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
-var index = require('../routes/index.js');
+var index = require('./routes/index.js');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
 app.use('/', index);
 
 var server = app.listen(3000, function(){
